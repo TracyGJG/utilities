eb199c322aa532ac0112b790cd2efef1a537d818
TGJ Gilmore <TracyGJG@users.noreply.github.com>
2024-04-19 16:48:56 +0100

    New LensFn function

--- changelog.md
+++ changelog.md
@@ -1,86 +1,90 @@
 # Change Log
 
+## Update 19th April 2024
+
+- New `lensFn` function added to the tools module.
+
 ## Update 23rd November 2023
 
--   New function added to the data-comparison module: `referencedClone`.
+- New function added to the data-comparison module: `referencedClone`.
 
 ## Update 11th November 2023
 
--   New function added to ancillaries module: `dateBasedRandom`.
+- New function added to ancillaries module: `dateBasedRandom`.
 
 ## Update 24th October 2023
 
--   New functions added to the tools module: `escapeRegExp`, `isRegExpPattern` and `regExpString`.
+- New functions added to the tools module: `escapeRegExp`, `isRegExpPattern` and `regExpString`.
 
 ## Update 6th October 2023
 
--   Replaced the `regExpFromString` function of the tools module for an improved `regExpTemplate` function.
--   Added the `flattenObject` function to the dataComparison module.
+- Replaced the `regExpFromString` function of the tools module for an improved `regExpTemplate` function.
+- Added the `flattenObject` function to the dataComparison module.
 
 ## Update 11th August 2023
 
--   Added the `permute` and `unflatten` functions to the Array section.
+- Added the `permute` and `unflatten` functions to the Array section.
 
 ## Update 10th June 2023
 
--   Added the `debounce` and `throttle` functions to the DOM section.
+- Added the `debounce` and `throttle` functions to the DOM section.
 
 ## Update 7th May 2023
 
--   Added the `regExpFromString`, `parseJson`, `stringifyJson` and `generateEnums` functions to the tools section.
+- Added the `regExpFromString`, `parseJson`, `stringifyJson` and `generateEnums` functions to the tools section.
 
 ## Update 5th May 2023
 
--   Added DOM functions inspired from the work of [Dave Gray](https://youtu.be/LDgPTw6tePk).
-    * Arrays
-      -   shuffleArray - Shuffle an array of numbers
-    * DOM
-      -   acc - add CSS class
-      -   dce - delete child elements
-      -   sui - sanitive untrusted/user input
-    * Data Comparison
-      -   isBase - confirms if the given datum has a value of null or undefined.
-      -   isObject - confirms if the given datum is an Object.
+- Added DOM functions inspired from the work of [Dave Gray](https://youtu.be/LDgPTw6tePk).
+  - Arrays
+    - shuffleArray - Shuffle an array of numbers
+  - DOM
+    - acc - add CSS class
+    - dce - delete child elements
+    - sui - sanitive untrusted/user input
+  - Data Comparison
+    - isBase - confirms if the given datum has a value of null or undefined.
+    - isObject - confirms if the given datum is an Object.
 - Tools
-  *   SIMD - refined.
+  - SIMD - refined.
 
 ## Update 29th April 2023
 
--   Range methods `rangeGenerator` added.
+- Range methods `rangeGenerator` added.
 
 ## Update 2nd April 2023
 
--   Array methods `batchBy.size` and `batchBy.number` added.
+- Array methods `batchBy.size` and `batchBy.number` added.
 
 ## Update 1st April 2023
 
--   `simd` added to tools.
--   `extractProperty` removed from dataComparison (duplicate of tools/`lens`).
+- `simd` added to tools.
+- `extractProperty` removed from dataComparison (duplicate of tools/`lens`).
 
 ## Update 17th March 2023
 
--   DOM manipulation functions added.
--   Clipboard functions `copy` and `paste` added.
--   Exercise function `consoleGroup` added.
+- DOM manipulation functions added.
+- Clipboard functions `copy` and `paste` added.
+- Exercise function `consoleGroup` added.
 
 * DOM manipulation functions inspired but Kyle Cook's YouTube video [Stop Wasting Your Time - Use These 16 JS Utility Functions Instead
-](https://youtu.be/EoUIS2PxKCs) on his [Web Dev Simplified](https://www.youtube.com/@WebDevSimplified) channel.
+  ](https://youtu.be/EoUIS2PxKCs) on his [Web Dev Simplified](https://www.youtube.com/@WebDevSimplified) channel.
 
 ## Update 7th March 2023
 
--   Added `isEmptyObject` function to Data Comparison group.
--   Added Ancillary functions `random` and `webStore`.
+- Added `isEmptyObject` function to Data Comparison group.
+- Added Ancillary functions `random` and `webStore`.
 
 ## Update 1st March 2023
 
--   Added the `mapGetter` function to the Ancillaries group.
+- Added the `mapGetter` function to the Ancillaries group.
 
 ## Update 4th January 2023
 
--   Added the `adhocArray` function to the Exercising group.
+- Added the `adhocArray` function to the Exercising group.
 
 ---
 
-* [Archived Change Log](archivedLog.md)
+- [Archived Change Log](archivedLog.md)
 
-* [Index](README.md)
+- [Index](README.md)
--- src/dataComparison/index.js
+++ src/dataComparison/index.js
@@ -17,141 +17,140 @@ export const DATA_TYPES = JSON.parse(`{
 }`);
 
 export function cloneObject(obj) {
-	if (obj === null || typeof obj !== 'object' || '__isActiveClone' in obj)
-		return obj;
+  if (obj === null || typeof obj !== 'object' || '__isActiveClone' in obj)
+    return obj;
 
-	let temp = obj instanceof Date ? new obj.constructor() : obj.constructor();
+  let temp = obj instanceof Date ? new obj.constructor() : obj.constructor();
 
-	for (let key in obj) {
-		if (Object.prototype.hasOwnProperty.call(obj, key)) {
-			obj['__isActiveClone'] = null;
-			temp[key] = cloneObject(obj[key]);
-			delete obj['__isActiveClone'];
-		}
-	}
-	return temp;
+  for (let key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      obj['__isActiveClone'] = null;
+      temp[key] = cloneObject(obj[key]);
+      delete obj['__isActiveClone'];
+    }
+  }
+  return temp;
 }
 
 export function compareObjectByProperty(propName, ascending = true) {
-	return (objA, objB) =>
-		(ascending ? 1 : -1) *
-		(objA[propName] < objB[propName]
-			? -1
-			: 1 * (objA[propName] > objB[propName]));
+  return (objA, objB) =>
+    (ascending ? 1 : -1) *
+    (objA[propName] < objB[propName]
+      ? -1
+      : 1 * (objA[propName] > objB[propName]));
 }
 
 export function dataType(subject) {
-	return Object.prototype.toString.call(subject).slice(8, -1).toLowerCase();
+  return Object.prototype.toString.call(subject).slice(8, -1).toLowerCase();
 }
 
 export function duplicateObject(srcObj) {
-	if (Array.isArray(srcObj)) {
-		return srcObj.map(duplicateObject);
-	}
-	if (srcObj != null && typeof srcObj === 'object') {
-		if (srcObj instanceof Date) {
-			return new Date(srcObj.toISOString());
-		}
-		if (srcObj instanceof RegExp) {
-			return new RegExp(srcObj.source, srcObj.flags);
-		}
-		return Object.entries(srcObj).reduce(
-			(tgtObj, [propName, propValue]) => ({
-				...tgtObj,
-				[propName]: duplicateObject(propValue),
-			}),
-			{}
-		);
-	} else {
-		return srcObj;
-	}
+  if (Array.isArray(srcObj)) {
+    return srcObj.map(duplicateObject);
+  }
+  if (srcObj != null && typeof srcObj === 'object') {
+    if (srcObj instanceof Date) {
+      return new Date(srcObj.toISOString());
+    }
+    if (srcObj instanceof RegExp) {
+      return new RegExp(srcObj.source, srcObj.flags);
+    }
+    return Object.entries(srcObj).reduce(
+      (tgtObj, [propName, propValue]) => ({
+        ...tgtObj,
+        [propName]: duplicateObject(propValue),
+      }),
+      {}
+    );
+  } else {
+    return srcObj;
+  }
 }
 
 export function flattenObject(srcObj) {
-	return Object.entries(srcObj).reduce(
-		(newObj, [key, value]) => _flattenObject(newObj, key, value),
-		{}
-	);
+  return Object.entries(srcObj).reduce(
+    (newObj, [key, value]) => _flattenObject(newObj, key, value),
+    {}
+  );
 
-	function _flattenObject(tgtObj, propName, propValue) {
-		if (Array.isArray(propValue)) {
-			propValue.forEach((arrValue, index) =>
-				_flattenObject(tgtObj, `${propName}[${index}]`, arrValue)
-			);
-		} else if (isObject(propValue) && !isEmptyObject(propValue)) {
-			Object.entries(propValue).forEach(([propKey, propVal]) =>
-				_flattenObject(tgtObj, `${propName}.${propKey}`, propVal)
-			);
-		} else {
-			console.log(`Primitive: ${propName} = ${propValue}`);
-			tgtObj[`${propName}`] = propValue;
-		}
-		return tgtObj;
-	}
+  function _flattenObject(tgtObj, propName, propValue) {
+    if (Array.isArray(propValue)) {
+      propValue.forEach((arrValue, index) =>
+        _flattenObject(tgtObj, `${propName}[${index}]`, arrValue)
+      );
+    } else if (isObject(propValue) && !isEmptyObject(propValue)) {
+      Object.entries(propValue).forEach(([propKey, propVal]) =>
+        _flattenObject(tgtObj, `${propName}.${propKey}`, propVal)
+      );
+    } else {
+      tgtObj[`${propName}`] = propValue;
+    }
+    return tgtObj;
+  }
 }
 
 export function isBase(val) {
-	return val == null;
+  return val == null;
 }
 
 export function isEmptyObject(obj) {
-	return (
-		obj && !Object.getOwnPropertyNames(obj).length && obj.constructor === Object
-	);
+  return (
+    obj && !Object.getOwnPropertyNames(obj).length && obj.constructor === Object
+  );
 }
 
 export function isObject(obj) {
-	return obj === Object(obj) && !Array.isArray(obj);
+  return obj === Object(obj) && !Array.isArray(obj);
 }
 
 export function objectEquality(obj1, obj2, testStructureOnly = false) {
-	const keysString = obj => Object.keys(obj).join();
-	const typeCompare = (val1, val2) => dataType(val1) === dataType(val2);
-	return deepEquality(obj1, obj2);
+  const keysString = obj => Object.keys(obj).join();
+  const typeCompare = (val1, val2) => dataType(val1) === dataType(val2);
+  return deepEquality(obj1, obj2);
 
-	function deepEquality(val1, val2) {
-		if (isObject(val1)) {
-			return (
-				isObject(val2) &&
-				keysString(val1) === keysString(val2) &&
-				Object.entries(val1).every(([key, val]) =>
-					deepEquality(val, val2?.[key])
-				)
-			);
-		}
-		if (Array.isArray(val1)) {
-			return (
-				Array.isArray(val2) &&
-				val1.length === val2.length &&
-				val1.every((val, idx) => deepEquality(val, val2?.[idx]))
-			);
-		}
-		return typeCompare(val1, val2) && (testStructureOnly || val1 === val2);
-	}
+  function deepEquality(val1, val2) {
+    if (isObject(val1)) {
+      return (
+        isObject(val2) &&
+        keysString(val1) === keysString(val2) &&
+        Object.entries(val1).every(([key, val]) =>
+          deepEquality(val, val2?.[key])
+        )
+      );
+    }
+    if (Array.isArray(val1)) {
+      return (
+        Array.isArray(val2) &&
+        val1.length === val2.length &&
+        val1.every((val, idx) => deepEquality(val, val2?.[idx]))
+      );
+    }
+    return typeCompare(val1, val2) && (testStructureOnly || val1 === val2);
+  }
 }
 
 export function referencedClone(src, propList = [], isInclude = false) {
-	if (!isObject(src)) return src;
-	return Object.entries(src).reduce((tgt, [key, val]) => {
-		if (isInclude === propList.includes(key)) {
-			if (_isObject(val)) {
-				tgt[key] = val;
-			} else {
-				Object.defineProperty(tgt, key, {
-					get() {
-						return src[key];
-					},
-					set(_val) {
-						src[key] = _val;
-					},
-					enumerable: true,
-				});
-			}
-		}
-		return tgt;
-	}, {});
+  if (!isObject(src)) return src;
+  return Object.entries(src).reduce((tgt, [key, val]) => {
+    if (isInclude === propList.includes(key)) {
+      if (_isObject(val)) {
+        tgt[key] = val;
+      } else {
+        Object.defineProperty(tgt, key, {
+          get() {
+            return src[key];
+          },
+          set(_val) {
+            src[key] = _val;
+          },
+          enumerable: true,
+        });
+      }
+    }
+    return tgt;
+  }, {});
 
-	function _isObject(obj) {
-		return !!obj && typeof obj === 'object';
-	}
+  function _isObject(obj) {
+    return !!obj && typeof obj === 'object';
+  }
 }
